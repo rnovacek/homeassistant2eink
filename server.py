5e54288c13c4c0dd84c53b7de36db09c69689eef
Radek Novacek <radek@novackovi.cz>
2024-10-17 12:30:32 +0200

    don't have browser permanently open

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+from contextlib import asynccontextmanager
 import logging
+from typing import Any
 from playwright.async_api import async_playwright, Page
 from litestar import Litestar, get, Request, Response
 from litestar.datastructures import State
-from pydantic import BaseSettings, Field
+from pydantic_settings import BaseSettings
 from PIL import Image
 from io import BytesIO
 
@@ -26,11 +28,13 @@ class Settings(BaseSettings):
     homeassistant_password: str = ''
 
     class Config:
+        extra = "ignore"
         env_file = '.env'
         env_file_encoding = 'utf-8'
 
 
-async def start_browser(state: State):
+@asynccontextmanager
+async def browser(state: State):
     settings: Settings = state['settings']
 
     playwright = await async_playwright().start()
@@ -43,15 +47,19 @@ async def start_browser(state: State):
     state['page'] = page
     state['browser'] = browser
 
-    await page.goto(settings.homeassistant_url)
-    await page.wait_for_selector('ha-authorize')
-    await page.wait_for_selector('.action mwc-button')
+    _ = await page.goto(settings.homeassistant_url)
+    _ = await page.wait_for_selector("ha-authorize")
+    _ = await page.wait_for_selector(".action mwc-button")
 
     await page.type('[name="username"]', settings.homeassistant_username)
     await page.type('[name="password"]', settings.homeassistant_password)
     await page.click('.action mwc-button')
 
-    await page.wait_for_selector('home-assistant-main #view')
+    _ = await page.wait_for_selector("home-assistant-main #view")
+
+    yield page
+
+    await browser.close()
 
 
 async def handle_notification(page: Page):
@@ -79,41 +87,39 @@ async def handle_notification(page: Page):
 
 
 async def get_screenshot(state: State):
-    page: Page = state['page']
+    async with browser(state) as page:
+        view = await page.wait_for_selector("home-assistant-main #view")
+        assert view
 
-    view = await page.wait_for_selector('home-assistant-main #view')
-    assert view
+        await handle_notification(page)
 
-    await handle_notification(page)
+        image_data = await page.screenshot(
+            type="png",
+            animations="disabled",
+            clip={
+                "x": SIDEBAR_WIDTH,
+                "y": HEADER_HEIGHT,
+                "width": VIEW_WIDTH,
+                "height": VIEW_HEIGHT,
+            },
+            timeout=10_000,
+        )
+        image_bytes = BytesIO(image_data)
 
-    image_data = await page.screenshot(
-        type='png',
-        animations='disabled',
-        clip={'x': SIDEBAR_WIDTH, 'y': HEADER_HEIGHT, 'width': VIEW_WIDTH, 'height': VIEW_HEIGHT},
-        timeout=10_000,
-    )
-    image_bytes = BytesIO(image_data)
-
-    image = Image.open(image_bytes)
-    new_image = (
-        image.
-        resize(size=(VIEW_WIDTH, VIEW_HEIGHT)).
-        convert('L').
-        rotate(90, expand=True)
-    )
-    image_bytes.seek(0)
-    new_image.save(image_bytes, format='PNG', quality=80, optimize=True)
-    print('Image ready, size:', len(image_bytes.getvalue()))
-    return image_bytes.getvalue()
-
-
-async def stop_browser(state: State):
-    browser = state['browser']
-    await browser.close()
+        image = Image.open(image_bytes)
+        new_image = (
+            image.resize(size=(VIEW_WIDTH, VIEW_HEIGHT))
+            .convert("L")
+            .rotate(90, expand=True)
+        )
+        _ = image_bytes.seek(0)
+        new_image.save(image_bytes, format="PNG", quality=80, optimize=True)
+        print("Image ready, size:", len(image_bytes.getvalue()))
+        return image_bytes.getvalue()
 
 
 @get("/")
-async def index(request: Request, state: State) -> Response:
+async def index(request: Request[Any, Any, Any], state: State) -> Response[Any]:
     auth = request.headers.get('Authorization')
     settings: Settings = state['settings']
     token = settings.token
@@ -126,12 +132,6 @@ async def index(request: Request, state: State) -> Response:
         return Response(await get_screenshot(state), media_type='image/png')
     except Exception as e:
         logger.exception(e)
-        try:
-            stop_browser(state)
-        except Exception as err:
-            logger.error('Unable to stop browser: %s', err)
-
-        start_browser(state)
         return Response('Error', status_code=500, media_type='text/plain')
 
 settings = Settings()
@@ -146,10 +146,9 @@ if not settings.homeassistant_password:
 
 app = Litestar(
     route_handlers=[index],
-    on_startup=[start_browser],
-    on_shutdown=[stop_browser],
-
-    state=State({
-        'settings': Settings(),
-    }),
+    state=State(
+        {
+            "settings": Settings(),
+        }
+    ),
 )
